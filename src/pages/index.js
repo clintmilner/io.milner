4cfa474093bae44e73ffe29e43e0d3d6eed85875
Clint Milner <clinton.milner@gmail.com>
2020-05-21 15:51:14 +0100

    fixed spelling in the about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default () => {
                         light, medium, and heavy traffic sites, as well as greenfield software solutions.</p>
                     <p>My past experience has included working on small teams that have grown into large ones, as
                         well as working independently on employer projects and freelance client work.</p>
-                    <p>Currently, I am a Senior UI Developer for a global tech company migrating a legacy management console to a single-page application using TypeScript at React.</p>
+                    <p>Currently, I am a Senior UI Developer for a global tech company migrating a legacy management console to a single-page application using TypeScript, React, and GraphQL.</p>
                     <p>Prior to my current role, I was the Head of Software Development managing a small team of software developers
                         building products in the Network Security space. I was 100% hands-on, and was responsible for
                         100% of the Front End code, but also had responsibilities in both the Full Stack, and
